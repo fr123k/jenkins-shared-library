9ed2cd5311bab482559d4029b302a2dbea86ef66
Frank Ittermann <frank.ittermann@yahoo.de>
2020-02-13 22:40:26 +0100

    Setup initial jenkins shared library structure

--- config/groovy/userPublicKeys.groovy
+++
@@ -1,8 +0,0 @@
-#! groovy
-
-def user = hudson.model.User.get('admin')
-
-def pubKey = new org.jenkinsci.main.modules.cli.auth.ssh.UserPropertyImpl(System.getenv()['JENKINS_CLI_PUBLIC_KEY'])
-user.addProperty(pubKey)
-
-user.save()
---
+++ vars/HelloWorld.groovy
@@ -0,0 +1,3 @@
+node('master') {
+    sh("echo hello world")
+}
