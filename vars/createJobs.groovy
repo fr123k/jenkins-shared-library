9fe3b6730c1f56b34147613d45a405fc569e83ef
Frank Ittermann <frank.ittermann@yahoo.de>
2020-04-04 23:25:21 +0200

    Add installation of plugins from jobDSL repository
    The jobDSL repository define needed plugins in the root of
    teh repository by placing a plugins.txt similar to the plugins.txt
    defined in the [jenkinci](https://github.com/jenkinsci/docker/blob/master/README.md) docker image.
    
    The defined plugins will only be installed if they are not
    already installed.

--- vars/createJobs.groovy
+++ vars/createJobs.groovy
@@ -1,3 +1,23 @@
+@NonCPS
+def installPlugins(String plugins) {
+    if (!plugins?.trim())
+        return
+    installedPlugins = jenkins.model.Jenkins.instance.getPluginManager().getPlugins().collect { it.getShortName() }
+
+    plugins.eachLine { line ->
+        if (line?.trim() && !line.startsWith( '#' )) {
+            foundPlugin = installedPlugins.findAll { key -> "${key}" == "${line}" }
+            echo "Check if plugin ${line} is installed"
+            if( !foundPlugin ){
+                echo "Install plugin ${line}"
+                Jenkins.instance.updateCenter.getPlugin(line).deploy(true)
+            } else {
+                echo "Skip plugin already installed ${line}"
+            }
+        }
+    }
+}
+
 def call(String repository="https://github.com/fr123k/jocker.git", String revision="master", String jobDSLPath="jenkins/jobDSL") {
     node {
         stage('Checkout') {
@@ -30,6 +50,11 @@ def call(String repository="https://github.com/fr123k/jocker.git", String revisi
                 removedJobAction: 'DELETE',
                 lookupStrategy: 'JENKINS_ROOT',
             )
+            if (fileExists('work/plugins.txt')) {
+                echo "check plugins.txt"
+                plugins = readFile "work/plugins.txt"
+                installPlugins(plugins)
+            }
         }
     }
 }
