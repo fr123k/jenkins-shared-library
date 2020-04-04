769708a31bfe16dc57ddc75d127a89ea579cb8f0
Frank Ittermann <frank.ittermann@yahoo.de>
2020-04-04 22:09:29 +0200

    Add parameters to createJobs function for generalization
    The createJobs method takes the following three arguments
    * repository: The repositoryi with jenkins jobDSL definition to
                  perform git clone.
    * revision: The git revision to checkout.
    * jobDSLPath: The path in this repository where the jenkins jobDSL
                  definitions files are located.

--- vars/createJobs.groovy
+++ vars/createJobs.groovy
@@ -1,13 +1,13 @@
-def call(arguments) {
+def call(String repository="https://github.com/fr123k/jocker.git", String revision="master", String jobDSLPath="jenkins/jobDSL") {
     node {
         stage('Checkout') {
             // TODO fix the git clone checkout mess to just clone a specific branch
             cleanWs()
             // git clone --branch ${arguments.revision} https://github.com/fr123k/jocker.git work --depth 1
             sh """
-            git clone https://github.com/fr123k/jocker.git work
+            git clone ${repository} work
             cd work
-            git checkout ${arguments.revision}
+            git checkout ${revision}
             cd ..
             """
         }
@@ -15,17 +15,20 @@ def call(arguments) {
             sh """
             ls -lha work
             """
+            // https://issues.jenkins-ci.org/browse/JENKINS-44142
+            // --> Note: when using multiple Job DSL build steps in a single job, set this to "Delete" only for the last Job DSL build step. 
+            // Otherwise views may be deleted and re-created. See JENKINS-44142 for details.
             jobDsl(
-                targets: 'work/shared-library/jobDSL/folders.groovy',
+                targets: "work/${jobDSLPath}/folders.groovy",
                 sandbox: false,
                 removedJobAction: 'IGNORE',
                 lookupStrategy: 'JENKINS_ROOT'
             )
             jobDsl(
-                targets: 'work/shared-library/jobDSL/*.groovy',
+                targets: "work/${jobDSLPath}/*.groovy",
                 sandbox: false,
                 removedJobAction: 'DELETE',
-                lookupStrategy: 'JENKINS_ROOT'
+                lookupStrategy: 'JENKINS_ROOT',
             )
         }
     }
