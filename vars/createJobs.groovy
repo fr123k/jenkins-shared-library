0de6845532540ca8a4ae1759982692bcdb52b97f
Frank Ittermann <frank.ittermann@yahoo.de>
2020-05-19 18:19:55 +0200

    Use the jenkins pipeline git step instead of git shell cmd

--- vars/createJobs.groovy
+++ vars/createJobs.groovy
@@ -23,41 +23,40 @@ def call(String repository="https://github.com/fr123k/jocker.git", String revisi
         stage('Checkout') {
             // TODO fix the git clone checkout mess to just clone a specific branch
             cleanWs()
-            // git clone --branch ${arguments.revision} https://github.com/fr123k/jocker.git work --depth 1
-            git branch: revision,
-                credentialsId:'deploy-key-shared-library',
-                url: repository
+            dir('work') {
+                if (revision.contains("/"))
+                    revision = revision.split("/", 2)[1]
 
-            // sh """
-            // git clone ${repository} work
-            // cd work
-            // git checkout ${revision}
-            // cd ..
-            // """
+                git branch: revision,
+                    credentialsId:'deploy-key-shared-library',
+                    url: repository
+            }
         }
         stage("Create jobs") {
-            sh """
-            ls -lha work
-            """
-            // https://issues.jenkins-ci.org/browse/JENKINS-44142
-            // --> Note: when using multiple Job DSL build steps in a single job, set this to "Delete" only for the last Job DSL build step. 
-            // Otherwise views may be deleted and re-created. See JENKINS-44142 for details.
-            jobDsl(
-                targets: "work/${jobDSLPath}/folders.groovy",
-                sandbox: false,
-                removedJobAction: 'IGNORE',
-                lookupStrategy: 'JENKINS_ROOT'
-            )
-            jobDsl(
-                targets: "work/${jobDSLPath}/*.groovy",
-                sandbox: false,
-                removedJobAction: removedJobAction,
-                lookupStrategy: 'JENKINS_ROOT',
-            )
-            if (fileExists('work/plugins.txt')) {
-                echo "check plugins.txt"
-                plugins = readFile "work/plugins.txt"
-                installPlugins(plugins)
+            dir('work') {
+                sh """
+                ls -lha
+                """
+                // https://issues.jenkins-ci.org/browse/JENKINS-44142
+                // --> Note: when using multiple Job DSL build steps in a single job, set this to "Delete" only for the last Job DSL build step. 
+                // Otherwise views may be deleted and re-created. See JENKINS-44142 for details.
+                jobDsl(
+                    targets: "${jobDSLPath}/folders.groovy",
+                    sandbox: false,
+                    removedJobAction: 'IGNORE',
+                    lookupStrategy: 'JENKINS_ROOT'
+                )
+                jobDsl(
+                    targets: "${jobDSLPath}/*.groovy",
+                    sandbox: false,
+                    removedJobAction: removedJobAction,
+                    lookupStrategy: 'JENKINS_ROOT',
+                )
+                if (fileExists('plugins.txt')) {
+                    echo "check plugins.txt"
+                    plugins = readFile "plugins.txt"
+                    installPlugins(plugins)
+                }
             }
         }
     }
